f052e9b6919577ef8c8c435abf1cb4c7b82e9ab2
unetamps <unedo.tampubolon@gmail.com>
2024-10-13 00:14:59 +0700

    add glbb

--- moveMatrix.js
+++ moveMatrix.js
@@ -6,12 +6,15 @@ function uniformStrightMove(
   movespeed,
   direction,
   angle,
+  acc,
+  time,
 ) {
+  var scale = 100;
   theta += 0.05;
-  moveX += movespeed * 2 * direction;
+  moveX += (movespeed * direction) / scale;
   angle += 5;
-  if (moveX > 5 || moveX < -5) {
-    direction *= -1;
+  if (moveX > 11) {
+    moveX = -11;
   }
   return {
     theta,
@@ -24,22 +27,60 @@ function uniformStrightMove(
   };
 }
 
-// function constantAcceleration(
-//   theta,
-//   moveX,
-//   moveY,
-//   moveZ,
-//   movespeed,
-//   direction,
-//   angle,
-// ) {}
-
-function verticalMove(theta, moveX, moveY, moveZ, movespeed, direction, angle) {
+function constantAcceleration(
+  theta,
+  moveX,
+  moveY,
+  moveZ,
+  movespeed,
+  direction,
+  angle,
+  acc,
+  time,
+) {
+  var scale = 100;
+  var prevmove =
+    (movespeed * (time - 0.01) + acc * Math.pow(time - 0.01, 2)) / scale;
   theta += 0.05;
-  moveY += movespeed * 3 * direction;
+  var calcuate = (movespeed * time + acc * Math.pow(time, 2)) / scale;
+  moveX = moveX + (calcuate - prevmove);
   angle += 5;
-  if (moveY > 4 || moveY < -4) {
-    direction *= -1;
+  if (moveX > 11) {
+    moveX = -11;
+  }
+  return {
+    theta,
+    angle,
+    direction,
+    moveX,
+    moveY,
+    moveZ,
+    movespeed,
+  };
+}
+
+function verticalMove(
+  theta,
+  moveX,
+  moveY,
+  moveZ,
+  movespeed,
+  direction,
+  angle,
+  acc,
+  time,
+) {
+  var scale = 100;
+  theta += 0.05;
+  console.log("time", time);
+  moveY = (movespeed * time - (acc * Math.pow(time, 2)) / 2) / 100;
+  angle += 5;
+  // console.log("time", time);
+  console.log("MoveY", moveY);
+  if (moveY <= -2) {
+    time = 0;
+    acc = 0;
+    moveY = -2;
   }
   return {
     theta,
